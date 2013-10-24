93d2fb3c3337fca636e5daa454ec0290e7a2ba20
lwoodson <lance@webmaneuvers.com>
2013-10-24 08:36:10 -0500

    Clear the matrix when hosts offline should be protected by locking
    This was an uncommitted local change that didn't make it into the
    previous pull request that merged the other locking changes into
    master.  https://github.com/PeopleAdmin/qtrix/pull/17
    
    Oops.

--- lib/qtrix.rb
+++ lib/qtrix.rb
@@ -220,8 +220,8 @@ module Qtrix
 
   def self.fetch_queues(hostname, workers)
     HostManager.ping(hostname)
-    clear_matrix_if_any_hosts_offline
     with_lock timeout: 5, on_timeout: last_result do
+      clear_matrix_if_any_hosts_offline
       debug("fetching #{workers} queue lists for #{hostname}")
       overrides_queues = Qtrix::Override.overrides_for(hostname, workers)
       debug("overrides for #{hostname}: #{overrides_queues}")
